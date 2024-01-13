c8a93761bb2c5cf6b241c184d110a433483138be
om shankar <om8003golu@gmail.com>
2024-01-13 20:06:25 +0530

    set up react routings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,24 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import HomeLayout from "./components/HomeLayout";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ client/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <div className="text-red-500">Register</div>;
+};
+
+export default Register;
