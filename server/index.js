6ebb961d5ec3dfef6be709dc62ee9dbda8d65e9c
om shankar <om8003golu@gmail.com>
2024-01-15 11:01:41 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
+import mongoose from "mongoose";
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -14,6 +15,7 @@ const port = process.env.PORT || 8000;
 
 const start = async () => {
   try {
+    await mongoose.connect(process.env.MONGO_URI);
     app.listen(port, () => console.log(`server is running on port ${port}...`));
   } catch (error) {
     console.log(error);
